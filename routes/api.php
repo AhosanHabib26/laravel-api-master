e4dc4a25d1d757845b1fa1b34bffd82b3dc0942d
Ahosan Habib <103899438+AhosanHabib26@users.noreply.github.com>
2024-05-07 13:13:29 +0600

    Add files via upload

---
+++ routes/api.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+
+});
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::get('user_details', 'Api\AuthController@details');
+    Route::get('logout', 'Api\AuthController@logout');
+    Route::get('blogs','Api\BlogController@index');
+    Route::post('blog','Api\BlogController@store');
+    Route::get('blogs/{id}','Api\BlogController@show');
+    Route::post('blogs/{id}','Api\BlogController@update');
+    Route::delete('blogs/{id}','Api\BlogController@destroy');
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'password'
+], function () {
+    Route::post('create', 'Api\PasswordResetController@create');
+    Route::get('find/{token}', 'Api\PasswordResetController@find');
+    Route::post('reset', 'Api\PasswordResetController@reset');
+});
+
+Route::post('login', 'Api\AuthController@login');
+Route::post('register', 'Api\AuthController@register');
+ Route::get('register/activate/{token}', 'Api\AuthController@registerActivate');
+
+ 
